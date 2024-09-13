ed1f467715bbcbb29c04178b8fdba528a370e49b
shoaib-1729 <shoaibahktar56@gmail.com>
2024-09-13 21:02:01 +0530

    event module

--- node_basics/lec11_event-module/index.js
+++ node_basics/lec11_event-module/index.js
@@ -1,11 +1,35 @@
-// OS Module
-import os from "os"
+import EventEmitter from "events"
 
-// console.log(os.platform());
-// console.log(os.arch());
-// console.log(os.cpus());
-// console.log(os.hostname());
-// console.log(os.homedir());
-// console.log(os.networkInterfaces());
-console.log(os.freemem());
-console.log(os.totalmem());
+// creating class
+class MyEmitter extends EventEmitter {}
+
+// creating object
+const myEmitterObj = new MyEmitter()
+
+// example 1
+// registering event listener using 'on' method
+myEmitterObj.on("event", function() {
+        console.log("An event occurred...!!!");
+    })
+    // trigger/fire/emit  the event that we have defined using emit
+myEmitterObj.emit("event")
+myEmitterObj.emit("event")
+
+// example 2
+// problem with emit method
+// let count = 0;
+// myEmitterObj.on("event", function() {
+//     console.log(++count);
+// })
+// myEmitterObj.emit("event")
+// myEmitterObj.emit("event")
+
+// example 3
+// emit the event only once using 'once' method
+// let count = 0;
+// myEmitterObj.once("event", function() {
+//         console.log(++count);
+//     })
+// event will trigger only once
+// myEmitterObj.emit("event")
+// myEmitterObj.emit("event")
