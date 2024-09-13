1386824a44a49bab21465909cf502f111dde2569
shoaib-1729 <shoaibahktar56@gmail.com>
2024-09-14 01:16:50 +0530

    routing and serving html pages

--- node_basics/lec15_routing-serving-html/index.js
+++ node_basics/lec15_routing-serving-html/index.js
@@ -1,16 +1,50 @@
-import http from "http";
+import http from "http"
+import fs from "fs"
 
-// creating web server using http
 const server = http.createServer((req, res) => {
-    res.setHeader("Content-Type", "text/plain")
-    res.end("Response from Sever")
+    // set the content type
+    res.writeHead(200, "OK", { "Content-Type": "text/html" })
+        // serving html
+        // res.end("<h1>Home Page</h1>")
+
+    // setting up routes or url handling
+    // if (req.url === '/') {
+    //     res.end("<h1>Home Page</h1>")
+    // } else if (req.url === '/about') {
+    //     res.end("<h1>About Page</h1>")
+    // } else {
+    //     res.end("<h1>404 Page Not Found!!</h1>")
+    // }
+
+    // serving html pages (file system is required to fetch files)
+    // home route
+    if (req.url === '/') {
+        fs.readFile("./public/home.html", (error, data) => {
+            if (error) {
+                throw error
+            }
+            // send the data
+            res.end(data)
+        })
+    }
+    // about route
+    else if (req.url === '/about') {
+        fs.readFile("./public/about.html", (error, data) => {
+            if (error) {
+                throw error
+            }
+            // send the data
+            res.end(data)
+        })
+    } else {
+        res.end("<h1>404 Page Not Found!!</h1>")
+    }
+
 })
 
-// making the port and host dynamic
-const PORT = process.env.PORT || 8000;
-const HOST = "localhost";
+const PORT = process.env.PORT || 8000
+const HOST = "localhost"
 
-// listening on port
 server.listen(PORT, HOST, () => {
     console.log("Server is running at http://localhost:8000");
 })
